4e7ed801c65e91a144f449c665b39b462af618c8
William-An <China_Aisa@live.com>
2020-03-04 15:57:37 -0500

    Project init

---
+++ src/main.c
@@ -0,0 +1,20 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f0xx.h"
+#include "stm32f0_discovery.h"
+			
+
+int main(void)
+{
+
+	for(;;);
+}
